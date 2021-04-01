415d9e2947fc13ecc32d130448082c61a8aa9f33
Fisherman7676 <graham.dean@sky.com>
2021-04-01 14:06:27 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,6 +1,6 @@
 import os
 
-ip_list = ["8.8.8.8","8.8.4.4","34.75.65.39","34.72.35.23"]
+ip_list = ["8.8.8.8","8.8.4.4","35.184.11.100"]
 
 for ip in ip_list:
     response = os.popen(f"ping {ip}").read()
